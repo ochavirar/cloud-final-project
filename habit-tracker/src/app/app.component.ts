921453d14a0ab87bb3921f808f09729b714f91e2
Omar Chavira <ochavirar@outlook.com>
2024-04-18 22:25:00 -0600

    Images

--- habit-tracker/src/app/app.component.ts
+++ habit-tracker/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ENVIRONMENT_INITIALIZER } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import AWSS3UploadAshClient from 'aws-s3-upload-ash';
+import { UploadResponse } from 'aws-s3-upload-ash/dist/types';
+import { environment }  from './../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +12,29 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'habit-tracker';
+  fileSelected:any = null;
+
+  config = {
+    bucketName: 'ppbucketcloud',
+    region: 'us-east-1',
+    accessKeyId: environment.AWS_ACCESS_KEY,
+    secretAccessKey: environment.AWS_SECRET_ACCESS_KEY,
+    s3Url: 'https://ppbucketcloud.s3.amazonaws.com/'
+  }
+
+  S3CustomClient: AWSS3UploadAshClient = new AWSS3UploadAshClient(this.config);
+
+  onChangeFile(event: any){
+    console.log(event.target.files[0]);
+    this.fileSelected = event.target.files[0];
+  }
+
+  async handleSendFile(){
+    console.log(environment);
+    console.log("handleSendFile");
+    await this.S3CustomClient
+      .uploadFile(this.fileSelected, this.fileSelected.type, undefined, this.fileSelected.name, "private")
+      .then((data: UploadResponse) => console.log(data))
+      .catch((err:any) => console.log(err));
+  }
 }
---
+++ habit-tracker/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    AWS_ACCESS_KEY: 'AKIATCKARB22HX4VZPIQ',
+    AWS_SECRET_ACCESS_KEY: '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'
+};
